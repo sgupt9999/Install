208b456a80a9ec20cf876070c401b32c8cc82d86
Cloud User <ec2-user@ip-172-31-19-235.us-west-2.compute.internal>
2018-04-15 15:49:49 +0000

    1st commit of the python install. Not completely workign yet

---
+++ python.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Installing the latest version of python on Centos/RHEL 7+
+
+
+if [[ $EUID != "0" ]]
+then
+	echo "ERROR. Need to have root privileges to run this script"
+	exit 1
+fi
+
+GROUPPACKAGES="development tools"
+PACKAGES="libffi-devel zlib-devel bzip2-devel openssl-devel ncurses-devel \
+	    sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel \
+	    expat-devel lsof wget words which"
+PYTHONFILENAME="Python-3.6.5.tar.xz"
+PYTHONFILEURL="https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tar.xz"
+PYTHONINSTALLDIR="/usr/src/Python-3.6.5"
+
+#yum group remove -y $GROUPPACKAGES
+yum group install -y $GROUPPACKAGES
+#yum remove -y $PACKAGES
+yum install -y $PACKAGES
+
+cd /usr/src
+rm -rf $PYTHONFILENAME
+rm -rf $PYTHONINSTALLDIR
+wget $PYTHONFILEURL
+tar -xf $PYTHONFILENAME
+cd $PYTHONINSTALLDIR
+./configure --enable-optimizations
+make altinstall
+
+
+
+cont="0"
+if [[ cont == "1" ]]
+then
+	pip3.6 install boto3
