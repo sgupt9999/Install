2f2e8bd97def2cc77ed2824f6582a3272ca3f62f
Cloud User <ec2-user@ip-172-31-21-31.us-west-2.compute.internal>
2019-02-19 04:26:51 +0000

    Downloadingversion 3.8 and also added pip upgrade command

--- python.sh
+++ python.sh
@@ -8,18 +8,20 @@ then
 	exit 1
 fi
 
-GROUPPACKAGES="development tools"
+GROUPPACKAGES="development tools" ## This installs a lot of dependecies that might be needed
 PACKAGES="libffi-devel zlib-devel bzip2-devel openssl-devel ncurses-devel \
 	    sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel \
 	    expat-devel lsof wget words which"
-PYTHONFILENAME="Python-3.6.5.tar.xz"
-PYTHONFILEURL="https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tar.xz"
-PYTHONINSTALLDIR="/usr/src/Python-3.6.5"
+PYTHONFILENAME="Python-3.6.8.tar.xz"
+PYTHONFILEURL="https://www.python.org/ftp/python/3.6.8/Python-3.6.8.tar.xz"
+PYTHONINSTALLDIR="/usr/src/Python-3.6.8"
 
 #yum group remove -y $GROUPPACKAGES
 yum group install -y $GROUPPACKAGES
 #yum remove -y $PACKAGES
 yum install -y $PACKAGES
+
+sed -i "s$.*secure_path.*$&:/usr/local/bin$" /etc/sudoers ## Add /usr/local/bin to the secure path. These are paths looked at when we run using sudo
 
 cd /usr/src
 rm -rf $PYTHONFILENAME
@@ -28,6 +30,7 @@ wget $PYTHONFILEURL
 tar -xf $PYTHONFILENAME
 cd $PYTHONINSTALLDIR
 ./configure --enable-optimizations
-make altinstall
+make altinstall ## This causes in to not replace the built in python execuatable
+pip3.6 install --upgrade pip
 
 
